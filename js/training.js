93407a4c4d3d0715400c138b8fd1062f4217ea19
Kun <ah09program@gmail.com>
2021-04-01 13:57:25 +0700

    them anh cho phan viet hoa va ki tu dac biet

--- js/training.js
+++ js/training.js
@@ -1,15 +1,16 @@
 
 img = document.querySelector(".content__img")
 let a = window.location.href.substr(41);
+if(a.search("%20")==0){
+    a = a.substr(3);
+    a =  [a.slice(0, 1), " ", a.slice(1)].join('');
+}
 console.log(a);
 
-if(a=="intro"){
+if(a[0]=="*"){
 }
 else{
-    if(a[0]==']'){
-
-    }
-    else if (a[0] == '['){
+    if(a[0]=='['){
 
     }
     else{
@@ -17,9 +18,11 @@ else{
         let  i = 0;
         let wrong = 0;
         let numberOfitem = a.length;
-        for(let i = 0; i < 50; i++) a+=a[Math.floor(Math.random()*numberOfitem)];
+        for(let i = 0; i < 75 +numberOfitem ; i++) a+=a[Math.floor(Math.random()*numberOfitem)];
+        a = a.substr(numberOfitem);
         for(let i = 0; i < a.length; i++){
-            train += `<span class="span">${a[i]}</span>`;
+            if (a[i]==" ") train += `<span class="span">&nbsp</span>`;
+            else train += `<span class="span">${a[i]}</span>`;
         }
         img.src = "../public/typing/"+ a.charCodeAt(0) +".png";
        // img.src = "../public/typing/"+ a.charCodeAt(i) + ".png";
@@ -28,20 +31,22 @@ else{
         text[0].classList.add('focus');
         text[0].classList.remove('span');
         window.addEventListener('keypress',(event)=>{
-            if(a[i]==event.key[0]){
+            console.log(event.key.charCodeAt(0));
+            console.log(event.key);
+            //if(a[i]==event.key[0]){
                 text[i].classList.add('correct');
                 ++i;
                 text[i].classList.add('focus');
                 text[i].classList.remove('span');
-               // console.log(event.key.charCodeAt(0));
+                //console.log(event.key.charCodeAt(0));
                 
-                img.src = "../public/typing/"+ a.charCodeAt(i) + ".png";        
-           }
-           else wrong++;
-           if(wrong > 10){
-            alert("Bạn gõ sai hơn 5 lần rồi. Thử lại nhá :))");
-            location.reload();
-            }
+                img.src = "../public/typing/"+ event.key.charCodeAt(0) + ".png";        
+          // }
+          // else wrong++;
+        //    if(wrong > 10){
+        //     alert("Bạn gõ sai hơn 10 lần rồi. Thử lại nhá :))");
+        //     location.reload();
+        //     }
         })
     }
 }
