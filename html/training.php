813f94fdb0738cdd3607334f341fa360b7228b3b
Lê Văn Trung <69235532+sevenghost-2001@users.noreply.github.com>
2021-04-07 22:39:42 +0700

    update lesson nangcao

--- html/training.php
+++ html/training.php
@@ -1,7 +1,14 @@
 <?php
 
 use function PHPSTORM_META\type;
-$lesson = ["fj","f j","dk","sl","a;","a sdfjkl;","asdf","jkl;","hg","a sdfghjkl;","ru","ei","wo","qp","q weruiop","qwer","uiop","ty","q wertyuiop","q werasdftg","y uiophjkl;","q wertyuiopasdfghjkl;","vm","c,","x.","z/","z xcvm,./","zxcv","m,./","bn","z xcvbnm,./","q wertasdfghzxcvb","y uiophjkl;nm,./","q wertyuiopasdfghjkl;zxcvbnm,./"]; 
+$lesson = ["fj","f j","dk","sl","a;","a sdfjkl;","asdf","jkl;","hg","a sdfghjkl;","ru","ei","wo","qp","q weruiop","qwer","uiop","ty","q wertyuiop","q werasdftg","y uiophjkl;","q wertyuiopasdfghjkl;","vm","c,","x.","z/","z xcvm,./","zxcv","m,./","bn","z xcvbnm,./","q wertasdfghzxcvb","y uiophjkl;nm,./","q wertyuiopasdfghjkl;zxcvbnm,./",
+"*Shift","FJjf","DKkd","SLsl","A:a;","A SDFJKL:asdfjkl;",
+"GHgh","A SDFGHJKL:asdfghjkl;","RUru","EIei","WOwo","QPqp","Q WERUIOPqweruiop",
+"TYty","Q WERTYUIOPqwertyuiop","VMvm","C<,c","X>x.","Z?z/",
+"Z XCVM<>?zxcvm,./","BNbn","Z XCVMBN<>?zxbncvm,./","47","38",
+"29","10","12","32","45","56","1 234567890","$&47","#*38","@(29",
+"!)10","! @#$&*()12347890","%^56","! @#$%^&*()1234567890",
+"1 234567890!@#$%^&*()qwertyuiopQWERTYUIOPasdfghjkl;ASDFGHJKL:zxcvbnm,./ZXCVBNM,./zxcvbnm<>?"]; 
 if (isset($_GET["lesson"]) && $lesson[$_GET["lesson"]]!=null) {
     $type = $_GET["lesson"];
 } else {
