d7a3677f10de5fd557b3781a6dd2b914c4b305f7
Lê Văn Trung <69235532+sevenghost-2001@users.noreply.github.com>
2021-04-08 10:39:01 +0700

    đã fix lesson(34-71)

--- html/training.php
+++ html/training.php
@@ -2,7 +2,7 @@
 
 use function PHPSTORM_META\type;
 $lesson = ["fj","f j","dk","sl","a;","a sdfjkl;","asdf","jkl;","hg","a sdfghjkl;","ru","ei","wo","qp","q weruiop","qwer","uiop","ty","q wertyuiop","q werasdftg","y uiophjkl;","q wertyuiopasdfghjkl;","vm","c,","x.","z/","z xcvm,./","zxcv","m,./","bn","z xcvbnm,./","q wertasdfghzxcvb","y uiophjkl;nm,./","q wertyuiopasdfghjkl;zxcvbnm,./",
-"*Shift","FJjf","DKkd","SLsl","A:a;","A SDFJKL:asdfjkl;",
+"FJjf","DKkd","SLsl","A:a;","A SDFJKL:asdfjkl;",
 "GHgh","A SDFGHJKL:asdfghjkl;","RUru","EIei","WOwo","QPqp","Q WERUIOPqweruiop",
 "TYty","Q WERTYUIOPqwertyuiop","VMvm","C<,c","X>x.","Z?z/",
 "Z XCVM<>?zxcvm,./","BNbn","Z XCVMBN<>?zxbncvm,./","47","38",
