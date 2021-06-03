1ac908e5718272004aeaa1319e43c52c65a47ecd
HuynhManDat <JuniorKevin110@gmail.com>
2021-06-04 00:08:05 +0700

    Push user center

--- html/profile2.php
+++ html/profile2.php
@@ -12,7 +12,7 @@
 
 <body>
     <?php
-        include './menu.php'
+    include './menu.php'
     ?>
     <main id="layout-main" class="group">
         <div id="layout-content" class="group">
@@ -24,6 +24,7 @@
                                 <div class="user-avatar">
                                     <img id="img-user" src="../public/avt/0.jpg">
                                 </div>
+                                <button>Thay đổi avatar</button>
                                 <span id="user-name" title="KevinHuynh">KevinHuynh</span>
                                 <div>
                                     <span id="number-coin"></span>
@@ -34,12 +35,9 @@
                                 </div>
                                 <div class="level-progress">
                                     <div class="current-progress">
-                                        <div class="standard-progress-bar"
-                                            style="position: relative; overflow: hidden;">
-                                            <div class="background"
-                                                style="overflow: hidden; width: 70%; position: absolute; left: 0px; top: 0px; height: 100%;">
-                                                <div class="background-render"
-                                                    style="background: linear-gradient(to right, rgb(92, 236, 209), rgb(49, 111, 196)); height: 100%; width: 204px;">
+                                        <div class="standard-progress-bar" style="position: relative; overflow: hidden;">
+                                            <div class="background" style="overflow: hidden; width: 70%; position: absolute; left: 0px; top: 0px; height: 100%;">
+                                                <div class="background-render" style="background: rgba(16, 215, 131, 1);height: 100%; width: 204px;">
                                                 </div>
                                             </div> <span class="text" style="z-index: 1;">50/71</span>
                                         </div>
@@ -63,69 +61,142 @@
                             <div id="infomation">
                                 <h3>
                                     Thông tin
-                                    <i data-toggle="modal" data-target="#modal-edit-achievement" id="edit-achievement"
-                                        class="cl-icon-pen pull-right" style="margin-right:12px;">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" width="24"
-                                            height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                    <i data-toggle="modal" data-target="#modal-edit-achievement" id="edit-achievement" class="cl-icon-pen pull-right" style="margin-right:12px;">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                         </svg>
                                     </i>
                                 </h3>
 
                                 <ul id="user-info-section">
-                                    <li><span><i class="cl-icon-envelope"><svg xmlns="http://www.w3.org/2000/svg"
-                                                    className="h-6 w-6" width="24" height="24" fill="none"
-                                                    viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                    <li><span><i class="cl-icon-envelope"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                                 </svg></i></span>
-                                        <span>Huỳnh Mẫn Đạt</span><span class="pull-right"><i
-                                                class="cl-icon-lock-alt"></i><i class="cl-icon-angle-down"></i></span>
+                                        <span>Huỳnh Mẫn Đạt</span><span class="pull-right"><i class="cl-icon-lock-alt"></i><i class="cl-icon-angle-down"></i></span>
                                     </li>
-                                    <li><span><i class="cl-icon-envelope"><svg xmlns="http://www.w3.org/2000/svg"
-                                                    className="h-6 w-6" width="24" height="24" fill="none"
-                                                    viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                    <li><span><i class="cl-icon-envelope"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                                 </svg></i></span>
-                                        <span>boy.cool46@yahoo.com</span><span class="pull-right"><i
-                                                class="cl-icon-lock-alt"></i><i class="cl-icon-angle-down"></i></span>
+                                        <span>boy.cool46@yahoo.com</span><span class="pull-right"><i class="cl-icon-lock-alt"></i><i class="cl-icon-angle-down"></i></span>
                                     </li>
-                                    <li><span><i class="cl-icon-envelope"><svg xmlns="http://www.w3.org/2000/svg"
-                                                    className="h-6 w-6" width="24" height="24" fill="none"
-                                                    viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-                                                </svg></i></span> <span>-</span><span class="pull-right"><i
-                                                class="cl-icon-lock-alt"></i><i class="cl-icon-angle-down"></i></span>
+                                    <li><span><i class="cl-icon-envelope"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                                                </svg></i></span> <span>-</span><span class="pull-right"><i class="cl-icon-lock-alt"></i><i class="cl-icon-angle-down"></i></span>
                                     </li>
-                                    <li><span><i class="cl-icon-envelope"><svg xmlns="http://www.w3.org/2000/svg"
-                                                    className="h-6 w-6" width="24" height="24" fill="none"
-                                                    viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                                        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                </svg></i></span> <span>-</span><span class="pull-right"><i
-                                                class="cl-icon-internet"></i><i class="cl-icon-angle-down"
-                                                aria-hidden="true"></i>
+                                    <li><span><i class="cl-icon-envelope"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                </svg></i></span> <span>-</span><span class="pull-right"><i class="cl-icon-internet"></i><i class="cl-icon-angle-down" aria-hidden="true"></i>
                                         </span>
                                     </li>
                                     <li>
-                                        <span><i class="cl-icon-envelope"><svg xmlns="http://www.w3.org/2000/svg"
-                                                    className="h-6 w-6" width="24" height="24" fill="none"
-                                                    viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                                        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                        <span><i class="cl-icon-envelope"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                                 </svg></i>
                                         </span>
                                         <span>2020-08-23</span>
-                                        <span class="pull-right"><i class="cl-icon-internet"></i><i
-                                                class="cl-icon-angle-down" aria-hidden="true"></i>
+                                        <span class="pull-right"><i class="cl-icon-internet"></i><i class="cl-icon-angle-down" aria-hidden="true"></i>
                                         </span>
                                     </li>
                                 </ul>
+                            </div>
+                        </div>
+                        <div id="cv-center" class="col-md-9 col-sm-12">
+                            <div id="setction-course" class="center-section">
+                                <h7>Hoạt động</h7>
+                                <div class="Luyentap">
+                                    <h6>Luyện tập</h6>
+                                    <table class="LuyenTapTable" style="width: 709px; height: 383px">
+                                        <thead>
+                                            <tr class="LuyenTaptr">
+                                                <?php
+                                                echo '<td class="LuyenTaptd">';
+                                                echo "Mon";
+                                                echo '<td class="LuyenTaptd">';
+                                                echo "Tue";
+                                                echo '<td class="LuyenTaptd">';
+                                                echo "Wed";
+                                                echo '<td class="LuyenTaptd">';
+                                                echo "Thu";
+                                                echo '<td class="LuyenTaptd">';
+                                                echo "Fri";
+                                                echo '<td class="LuyenTaptd">';
+                                                echo "Sat";
+                                                echo '<td class="LuyenTaptd">';
+                                                echo "Sun";
+                                                ?>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="LuyenTapTbody">
+                                            <?php for ($i = 1; $i <= 5; $i++) { ?>
+                                                <tr class="LuyenTaptr">
+                                                    <?php for ($j = 2; $j <= 8; $j++) {
+                                                        echo '<td class="LuyenTaptd">';
+                                                    }
+                                                    ?>
+                                                </tr>
+                                            <?php
+                                            }
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="Hoctap">
+                                    <div class="Hoctapcoban">
+                                        <h6>Học tập cơ bản</h6>
+                                        <table style="width: 330px; height: 352px">
+                                            <?php $dem = 0;
+                                            for ($i = 1; $i <= 6; $i++) {
+                                            ?>
+                                                <tr class="Hoctaptr">
+                                                    <?php for ($j = 1; $j <= 6; $j++) {
+                                                        if ($i == 6 && $j == 5) break;
+                                                        $dem++;
+                                                        echo '<td class="Hoctaptd">';
+                                                        if($dem==1){
+                                                            echo '<span class="color">';
+                                                            echo "$dem";
+                                                            echo '</span><div class="percent">99.23% <br> 11/12/2021</div> </td>';
+                                                        }
+                                                        else{
+                                                            echo '<span>';
+                                                            echo "$dem";
+                                                            echo '</span> </td>';
+                                                        }
+                                                        
+                                                        
+                                                    }
+                                                    ?>
+                                                </tr>
+                                            <?php
+                                            }
+                                            ?>
+                                        </table>
+                                    </div>
+                                    <div class="Hoctapnangcao">
+                                        <h6>Học tập nâng cao</h6>
+                                        <table style="width: 330px; height: 352px">
+                                            <?php
+                                            for ($i = 1; $i <= 7; $i++) {
+
+                                            ?>
+                                                <tr class="Hoctaptr">
+                                                    <?php for ($j = 1; $j <= 6; $j++) {
+                                                        if ($i == 7 && $j == 3) break;
+                                                        $dem++;
+                                                        echo '<td class="Hoctaptd"> <span>';
+                                                        echo "$dem";
+                                                        echo '</span> </td>';
+                                                    }
+                                                    ?>
+                                                </tr>
+                                            <?php
+                                            }
+                                            ?>
+                                        </table>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
                     </div>
